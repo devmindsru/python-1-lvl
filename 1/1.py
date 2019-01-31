b455b853f81f342407eeb51ac9a9d32339852143
devmindsru <devminds@mail.ru>
2019-01-31 19:56:40 +0300

    1

---
+++ 1/1.py
@@ -0,0 +1,53 @@
+# Easy
+# 1
+# name = input('Введите имя ')
+# print('Ваше зовут ' + name)
+# 2
+# num = int(input('Введите число '))
+# namber = num + 2
+# print(namber)
+# 3
+# age = int(input('Введите ваш возраст'))
+# if age >= 18:
+#     print('Доступ разрешен')
+# else:
+#     print('Извините, пользование данным ресурсом только с 18 лет')
+# Normal
+# 1
+# num = int(input('Введите число в диапазоне от 0 до 10 '))
+# numTrip = num * num
+# if (num <= 10 or num != 0) and num > 0:
+#     print(numTrip)
+# else:
+#     print('Вы ввели не подходящее число')
+# 2
+
+# num1 = input('Введите первое число - ')
+# num2 = input('Введите второе число - ')
+# print('Первое число ровно ' + num1 )
+# print('Второе число число ровно ' + num2)
+# print('Ахалай Махалай')
+# num3 = num2
+# num2 = num1
+# num1 = num3
+# print('Первое число теперь ровно ' + num1)
+# print('Второе число число теперь ровно ' +  num2)
+
+
+# Hard
+# name = input('Введите ваше имя ')
+# lastName = input('Введите вашу фамилию ')
+# name = 'Anton'
+# lastName = 'Musannikov'
+# age = int(input('Введите ваш возраст '))
+# mass = int(input('Введите ваш вес '))
+#
+# if age < 30 and 50 <= mass < 120:
+#     print('Вы в отличной форме', name, age)
+# elif age > 40 and (mass < 50 or mass > 120):
+#     print('Вам бы к врачу!')
+# elif age > 30 and (mass < 50 or mass > 120):
+#     print('Вам бы спортом заняться!')
+# else:
+#     print('Знаете мой врачебный опыт ограничивается примитивной формулой основанной на весе и '
+#           'возрасте, приходите, когда будете подходить под эту формулу!')
